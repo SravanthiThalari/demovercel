f32af34cfdedc8288326d43a920afccfb5c46a11
SravanthiThalari <23eg104c32@anurag.edu.in>
2026-03-11 13:40:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function Reg() {
-  return <h1>Registration Page</h1>;
-}
-
-function Home() {
-  return <h1>Home Page</h1>;
-}
+import Reg from "./Reg";
+import Home from "./Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/reg" element={<Reg />} />
-        <Route path="/h" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
